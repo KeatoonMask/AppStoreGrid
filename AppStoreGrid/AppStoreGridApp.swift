fb91794b8e444770785a178d3d325b503088a154
KeatoonMask <ennes.finsi@gmail.com>
2020-11-07 13:37:32 +0100

    Initial commit

---
+++ AppStoreGrid/AppStoreGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreGridApp.swift
+//  AppStoreGrid
+//
+//  Created by Serafín Ennes Moscoso on 07/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct AppStoreGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
